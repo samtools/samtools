5fb51bd0f26c5a61c3b26bd4092459e96c6286f8
Heng Li <lh3@live.co.uk>
2009-04-28 12:28:50 +0000

    fixed various issues about the GNU building scripts

--- cleanup.sh
+++ cleanup.sh
@@ -2,11 +2,3 @@ if [ -f "Makefile" ]; then
 	make distclean
 fi
 rm -fr *~ .in .gdb_history Makefile.in aclocal.m4 configure autom4*.cache config.guess config.h.in config.sub depcomp install-sh missing mkinstalldirs
-if [ -f "Makefile" ]; then
-	make distclean
-fi
-rm -fr *~ .in .gdb_history Makefile.in aclocal.m4 configure autom4*.cache config.guess config.h.in config.sub depcomp install-sh missing mkinstalldirs
-if [ -f "Makefile" ]; then
-	make distclean
-fi
-rm -fr *~ .in .gdb_history Makefile.in aclocal.m4 configure autom4*.cache config.guess config.h.in config.sub depcomp install-sh missing mkinstalldirs
