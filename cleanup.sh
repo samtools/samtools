e8865fa8476a34b6c7449fabe7d0a0b00a927e52
Heng Li <lh3@live.co.uk>
2009-04-28 12:43:33 +0000

    added missing files

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 if [ -f "Makefile" ]; then
 	make distclean
 fi
-rm -fr *~ .in .gdb_history Makefile.in aclocal.m4 configure autom4*.cache config.guess config.h.in config.sub depcomp install-sh missing mkinstalldirs
+rm -fr *~ .in .gdb_history Makefile.in aclocal.m4 configure autom4*.cache config.guess config.h.in config.sub depcomp install-sh missing mkinstalldirs misc/Makefile.in
