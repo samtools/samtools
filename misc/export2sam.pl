9cbf6422499eaa8dbb09ff9b873306202be5872f
Heng Li <lh3@live.co.uk>
2011-03-18 19:23:06 +0000

    export2sam.pl-2.3.1, provided by Chris Saunders from Illumina Inc.

--- misc/export2sam.pl
+++ misc/export2sam.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 #
 #
-# Script to convert GERALD export files to SAM format.
+# export2sam.pl converts GERALD export files to SAM format.
 #
 #
 #
@@ -9,8 +9,8 @@
 #
 # The MIT License
 #
-# Original SAMtools version 0.1.2 copyright (c) 2008-2009 Genome Research Ltd.
-# Modifications from version 0.1.2 to 2.0.0 copyright (c) 2010 Illumina, Inc.
+# Original SAMtools work copyright (c) 2008-2009 Genome Research Ltd.
+# Modified SAMtools work copyright (c) 2010 Illumina, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -32,33 +32,67 @@
 #
 #
 #
+#
 ########## ChangeLog:
 #
+# Version: 2.3.1 (18MAR2011)
+#
+#   - Restore file '-' as stdin input.
+#
+# Version: 2.3.0 (24JAN2011)
+#
+#   - Add support for export reserved chromosome name "CONTROL",
+#       which is translated to optional field "XC:Z:CONTROL".
+#   - Check for ".gz" file extension on export files and open
+#       these as gzip pipes when the extension is found.
+#
+# Version: 2.2.0 (16NOV2010)
+#
+#   - Remove any leading zeros in export fields: RUNNO,LANE,TILE,X,Y
+#   - For export records with reserved chromosome name identifiers
+#       "QC" and "RM", add the optional field "XC:Z:QC" or "XC:Z:RM"
+#       to the SAM record, so that these cases can be distinguished
+#       from other unmatched reads.
+#
+# Version: 2.1.0 (21SEP2010)
+#
+#   - Additional export record error checking.
+#   - Convert export records with chromomsome value of "RM" to unmapped 
+#       SAM records.
+#
 # Version: 2.0.0 (15FEB2010)
-#   Script updated by Illumina in conjunction with CASAVA 1.7.0 release.
+#
+#   Script updated by Illumina in conjunction with CASAVA 1.7.0
+#   release.
+#
 #   Major changes are as follows:
-#   - The CIGAR string has been updated to include all gaps from ELANDv2 alignments.
-#   - The ELAND single read alignment score is always stored in the optional "SM" field
-#       and the ELAND paired read alignment score is stored in the optional "AS" field
-#       when it exists.
-#   - The MAPQ value is set to the higher of the two alignment scores, but no greater
-#       than 254,  i.e. min(254,max(SM,AS))
-#   - The SAM "proper pair" bit (0x0002) is now set for read pairs meeting ELAND's
-#       expected orientation and insert size criteria.
-#   - The default quality score translation is set for export files which contain
-#       Phread+64 quality values. An option, "--qlogodds", has been added to
-#       translate quality values from the Solexa+64 format used in export files prior
-#       to Pipeline 1.3
-#   - The export match descriptor is now reverse-complemented when necessary such that
-#       it always corresponds to the forward strand of the reference, to be consistent
-#       with other information in the SAM record. It is now written to the optional
-#       'XD' field (rather than 'MD') to acknowledge its minor differences from the 
-#       samtools match descriptor (see additional detail below).
-#   - An option, "--nofilter", has been added to include reads which have failed
-#       primary analysis quality filtration. Such reads will have the corresponding
-#       SAM flag bit (0x0200) set.
-#   - Labels in the export 'contig' field are preserved by setting RNAME to
-#       "$export_chromosome/$export_contig" when then contig label exists.
+#   - The CIGAR string has been updated to include all gaps from
+#       ELANDv2 alignments.
+#   - The ELAND single read alignment score is always stored in the
+#       optional "SM" field and the ELAND paired read alignment score
+#       is stored in the optional "AS" field when it exists.
+#   - The MAPQ value is set to the higher of the two alignment scores,
+#       but no greater than 254, i.e. min(254,max(SM,AS))
+#   - The SAM "proper pair" bit (0x0002) is now set for read pairs
+#       meeting ELAND's expected orientation and insert size criteria.
+#   - The default quality score translation is set for export files
+#       which contain Phread+64 quality values. An option,
+#       "--qlogodds", has been added to translate quality values from
+#       the Solexa+64 format used in export files prior to Pipeline
+#       1.3
+#   - The export match descriptor is now reverse-complemented when
+#       necessary such that it always corresponds to the forward
+#       strand of the reference, to be consistent with other
+#       information in the SAM record. It is now written to the
+#       optional 'XD' field (rather than 'MD') to acknowledge its
+#       minor differences from the samtools match descriptor (see
+#       additional detail below).
+#   - An option, "--nofilter", has been added to include reads which
+#       have failed primary analysis quality filtration. Such reads
+#       will have the corresponding SAM flag bit (0x0200) set.
+#   - Labels in the export 'contig' field are preserved by setting
+#       RNAME to "$export_chromosome/$export_contig" when the contig
+#       label exists.
 #
 #
 # Contact: lh3
@@ -68,33 +102,49 @@
 #
 ########## Known Conversion Limitations:
 #
-# - Export records for reads that map to a position < 1 (allowed in export format), are converted
-#     to unmapped reads in the SAM record.
-# - Export records contain the reserved chromosome names: "NM" and "QC". "NM" indicates that the
-#     aligner could not map the read to the reference sequence set, and "QC" means that the 
-#     aligner did not attempt to map the read due to some technical limitation. Both of these 
-#     alignment types are collapsed to the single unmapped alignment state in the SAM record.
-# - The export match descriptor is slightly different than the samtools match descriptor. For
-#     this reason it is stored in the optional SAM field 'XD' (and not 'MD'). Note that the
-#     export match descriptor differs from the samtools version in two respects: (1) indels 
-#     are explicitly closed with the '$' character and (2) insertions must be enumerated in
-#     the match descriptor. For example a 35-base read with a two-base insertion is described
-#     as: 20^2$14
+# - Export records for reads that map to a position < 1 (allowed
+#     in export format), are converted to unmapped reads in the SAM
+#     record.
+# - Export records contain the reserved chromosome names: "NM",
+#     "QC","RM" and "CONTROL". "NM" indicates that the aligner could
+#     not map the read to the reference sequence set. "QC" means that
+#     the aligner did not attempt to map the read due to some
+#     technical limitation. "RM" means that the read mapped to a set
+#     of 'contaminant' sequences specified in GERALD's RNA-seq
+#     workflow. "CONTROL" means that the read is a control. All of
+#     these alignment types are collapsed to the single unmapped
+#     alignment state in the SAM record, but the optional SAM "XC"
+#     field is used to record the original reserved chromosome name of
+#     the read for all but the "NM" case.
+# - The export match descriptor is slightly different than the
+#     samtools match descriptor. For this reason it is stored in the
+#     optional SAM field 'XD' (and not 'MD'). Note that the export
+#     match descriptor differs from the samtools version in two
+#     respects: (1) indels are explicitly closed with the '$'
+#     character and (2) insertions must be enumerated in the match
+#     descriptor. For example a 35-base read with a two-base insertion
+#     is described as: 20^2$14
 #
 #
 #
 
-my $version = "2.0.0";
+my $version = "2.3.1";
 
 use strict;
 use warnings;
 
-use File::Spec qw(splitpath);
 use Getopt::Long;
+use File::Spec;
 use List::Util qw(min max);
 
 
 use constant {
+  EXPORT_MACHINE => 0,
+  EXPORT_RUNNO => 1,
+  EXPORT_LANE => 2,
+  EXPORT_TILE => 3,
+  EXPORT_X => 4,
+  EXPORT_Y => 5,
   EXPORT_INDEX => 6,
   EXPORT_READNO => 7,
   EXPORT_READ => 8,
@@ -107,6 +157,7 @@ use constant {
   EXPORT_SEMAP => 15,
   EXPORT_PEMAP => 16,
   EXPORT_PASSFILT => 21,
+  EXPORT_SIZE => 22,
 };
 
 
@@ -142,8 +193,7 @@ sub export2sam {
 
   my $cmdline = $0 . " " . join(" ",@ARGV);
   my $arg_count = scalar @ARGV;
-  my @spval = File::Spec->splitpath($0);
-  my $progname = $spval[2];
+  my $progname = (File::Spec->splitpath($0))[2];
 
   my $is_logodds_qvals = 0; # if true, assume files contain logodds (i.e. "solexa") quality values
   my $is_nofilter = 0;
@@ -165,11 +215,15 @@ $progname converts GERALD export files to SAM format.
 
 Usage: $progname --read1=FILENAME [ options ] | --version | --help
 
-  --read1=FILENAME  read1 export file (mandatory)
-  --read2=FILENAME  read2 export file
-  --nofilter        include reads that failed the pipeline/RTA purity filter
-  --qlogodds        assume export file(s) use logodds quality values as reported
-                      by pipeline prior to v1.3 (default: phred quality values)
+  --read1=FILENAME  read1 export file or '-' for stdin (mandatory)
+                      (file may be gzipped with ".gz" extension)
+  --read2=FILENAME  read2 export file or '-' for stdin
+                      (file may be gzipped with ".gz" extension)
+  --nofilter        include reads that failed the basecaller
+                      purity filter
+  --qlogodds        assume export file(s) use logodds quality values
+                      as reported by OLB (Pipeline) prior to v1.3
+                      (default: phred quality values)
 
 END
 
@@ -181,7 +235,7 @@ END
 
   if((not $result) or $help or ($arg_count==0)) {
     die($usage);
-  }  
+  }
 
   if(@ARGV) {
     print STDERR "\nERROR: Unrecognized arguments: " . join(" ",@ARGV) . "\n\n";
@@ -197,12 +251,31 @@ END
     die($usage);
   }
 
+  unless((-f $read1file) or ($read1file eq '-')) {
+    die("\nERROR: Can't find read1 export file: '$read1file'\n\n");
+  }
+
+  if (defined $read2file) {
+    unless((-f $read2file) or ($read2file eq '-')) {
+      die("\nERROR: Can't find read2 export file: '$read2file'\n\n");
+    }
+    if($read1file eq $read2file) {
+      die("\nERROR: read1 and read2 export filenames are the same: '$read1file'\n\n");
+    }
+  }
+
   my ($fh1, $fh2, $is_paired);
 
-  open($fh1, $read1file) or die("\nERROR: Can't open read1 export file: $read1file\n\n");
+  my $read1cmd="$read1file";
+  $read1cmd = "gzip -dc $read1file |" if($read1file =~ /\.gz$/);
+  open($fh1, $read1cmd)
+      or die("\nERROR: Can't open read1 process: '$read1cmd'\n\n");
   $is_paired = defined $read2file;
   if ($is_paired) {
-    open($fh2, $read2file) or die("\nERROR: Can't open read2 export file: $read2file\n\n");
+    my $read2cmd="$read2file";
+    $read2cmd = "gzip -dc $read2file |" if($read2file =~ /\.gz$/);
+    open($fh2, $read2cmd)
+        or die("\nERROR: Can't open read2 process: '$read2cmd'\n\n");
   }
   # quality value conversion table
   my @conv_table;
@@ -232,7 +305,7 @@ END
         die("\nERROR: read1 and read2 export files do not contain the same number of reads.\n  Extra reads observed in read1 file at line no: $export_line_count.\n\n");
       }
       &export2sam_aux($read2line, $export_line_count, \@s2, \@conv_table, $is_paired, 2, $is_nofilter);
-       
+
       if (@s1 && @s2) { # then set mate coordinate
         if($s1[SAM_QNAME] ne $s2[SAM_QNAME]){
           die("\nERROR: Non-paired reads in export files on line: $export_line_count.\n  Read1: $_  Read2: $read2line\n");
@@ -275,11 +348,18 @@ sub export2sam_aux {
   my ($line, $line_no, $s, $ct, $is_paired, $read_no, $is_nofilter) = @_;
   chomp($line);
   my @t = split("\t", $line);
+  if(scalar(@t) < EXPORT_SIZE) {
+    my $msg="\nERROR: Unexpected number of fields in export record on line $line_no of read$read_no export file. Found " . scalar(@t) . " fields but expected " . EXPORT_SIZE . ".\n";
+    $msg.="\t...erroneous export record:\n" . $line . "\n\n";
+    die($msg);
+  }
   @$s = ();
   my $isPassFilt = ($t[EXPORT_PASSFILT] eq 'Y');
   return if(not ($isPassFilt or $is_nofilter));
   # read name
-  $s->[SAM_QNAME] = $t[1]? "$t[0]_$t[1]:$t[2]:$t[3]:$t[4]:$t[5]" : "$t[0]:$t[2]:$t[3]:$t[4]:$t[5]";
+  my $samQnamePrefix = $t[EXPORT_MACHINE] . (($t[EXPORT_RUNNO] ne "") ? "_" .  int($t[EXPORT_RUNNO]) : "");
+  $s->[SAM_QNAME] = join(':', $samQnamePrefix, int($t[EXPORT_LANE]), int($t[EXPORT_TILE]),
+                         int($t[EXPORT_X]), int($t[EXPORT_Y]));
   # initial flag (will be updated later)
   $s->[SAM_FLAG] = 0;
   if($is_paired) {
@@ -317,8 +397,12 @@ sub export2sam_aux {
   # coor
   my $has_coor = 0;
   $s->[SAM_RNAME] = "*";
-  if ($t[EXPORT_CHROM] eq 'NM' or $t[EXPORT_CHROM] eq 'QC') {
+  if (($t[EXPORT_CHROM] eq 'NM') or
+      ($t[EXPORT_CHROM] eq 'QC') or
+      ($t[EXPORT_CHROM] eq 'RM') or
+      ($t[EXPORT_CHROM] eq 'CONTROL')) {
     $s->[SAM_FLAG] |= 0x4; # unmapped
+    push(@$s,"XC:Z:".$t[EXPORT_CHROM]) if($t[EXPORT_CHROM] ne 'NM');
   } elsif ($t[EXPORT_CHROM] =~ /(\d+):(\d+):(\d+)/) {
     $s->[SAM_FLAG] |= 0x4; # TODO: should I set BAM_FUNMAP in this case?
     push(@$s, "H0:i:$1", "H1:i:$2", "H2:i:$3")
@@ -371,7 +455,7 @@ sub export2sam_aux {
     # The export match descriptor differs slightly from the samtools match descriptor.
     # In order for the converted SAM files to be as compliant as possible,
     # we put the export match descriptor in optional field 'XD' rather than 'MD':
-    push(@$s, "XD:Z:$matchDesc"); 
+    push(@$s, "XD:Z:$matchDesc");
     push(@$s, "SM:i:$t[EXPORT_SEMAP]") if ($t[EXPORT_SEMAP] ne '');
     push(@$s, "AS:i:$t[EXPORT_PEMAP]") if ($is_paired and ($t[EXPORT_PEMAP] ne ''));
   }
@@ -379,7 +463,7 @@ sub export2sam_aux {
 
 
 
-# 
+#
 # the following code is taken from Richard Shaw's sorted2sam.pl file
 #
 sub reverse_compl_match_descriptor($)
