c8c818fc76e090e15d6b75ad442554b8af330cfb
Martin Pollard <mp15@sanger.ac.uk>
2013-07-04 16:41:21 +0100

    More work on rtrans_build unit test. Update test makefile to include rtrans_build test. Add additional sam test file for merge to exercise missing target @SQ, especially in region merge.

--- test/merge/test_rtrans_build.c
+++ test/merge/test_rtrans_build.c
@@ -3,14 +3,28 @@
 void dump_rtrans(int* rtrans, int n, int n_targets) {
 	printf("->n_targets:(%d)\n", n_targets);
 	int i, j;
-	for (i = 0; i < n_targets; ++i) {
-		for (j = 0; j < n; ++j)
-			fprintf(stderr, "%x",i,rtrans[i*n_targets+n]);
+	for (i = 0; i < n; ++i) {
+		fprintf(stderr, "%d",rtrans[i*n_targets+0]);
+		for (j = 1; j < n_targets; ++j)
+			fprintf(stderr, "\t%d",rtrans[i*n_targets+j]);
 		fprintf(stderr, "\n");
 	}
 }
 
 void setup_test_1(trans_tbl_t* tbl) {
+	tbl[0].n_targets = 2;
+	tbl[0].tid_trans = calloc(sizeof(int), 2);
+	tbl[0].tid_trans[0] = 0;
+	tbl[0].tid_trans[1] = 1;
+	tbl[0].rg_trans = kh_init(c2c);
+	tbl[0].pg_trans = kh_init(c2c);
+
+	tbl[1].n_targets = 2;
+	tbl[1].tid_trans = calloc(sizeof(int), 2);
+	tbl[1].tid_trans[0] = 1;
+	tbl[1].tid_trans[1] = 2;
+	tbl[1].rg_trans = kh_init(c2c);
+	tbl[1].pg_trans = kh_init(c2c);
 }
 
 bool check_test_1(trans_tbl_t* tbl, int* rtrans) {
@@ -43,24 +57,25 @@ int main(int argc, char**argv)
 
 	if (verbose) printf("BEGIN test 1\n");
 	// setup
-	trans_tbl_t tbl_1;
+	trans_tbl_t tbl_1[2];
+	int n_targets_1 = 3;
+	int n_1 = 2;
 	int* rtrans_1 = NULL;
-	setup_test_1(&tbl_1);
+	setup_test_1(&tbl_1[0]);
 	// test
 	if (verbose) {
-		printf("rtrans\n");
-		dump_rtrans(rtrans_1, 1, 1);
+		// dump_trans_tid
 	}
 	if (verbose) printf("RUN test 1\n");
-	rtrans_1 = rtrans_build(1, 1, NULL);
+	rtrans_1 = rtrans_build(n_1, n_targets_1, &tbl_1[0]);
 	if (verbose) printf("END RUN test 1\n");
 	if (verbose) {
 		printf("rtrans\n");
-		dump_rtrans(rtrans_1, 1, 1);
+		dump_rtrans(rtrans_1, n_1, n_targets_1);
 	}
-	if (check_test_1(&tbl_1, rtrans_1)) { ++success; } else { ++failure; }
+	if (check_test_1(&tbl_1[0], rtrans_1)) { ++success; } else { ++failure; }
 	// teardown
-	trans_tbl_destroy(&tbl_1);
+	trans_tbl_destroy(&tbl_1[0]);
 	free(rtrans_1);
 	if (verbose) printf("END test 1\n");
 	
