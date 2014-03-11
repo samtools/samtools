3f9224859e8acd78669f386b65281904b2af0375
John Marshall <jm18@sanger.ac.uk>
2014-03-11 16:34:14 +0000

    Fix buffer overflow
    Razip is on life support in this branch, but cherry-pick this from the
    standalone branch anyway, in case anyone happens to use this code.

--- razip.c
+++ razip.c
@@ -80,7 +80,7 @@ int main(int argc, char **argv)
 			if(pstdout){
 				f_dst = fileno(stdout);
 			} else {
-				char *name = malloc(sizeof(strlen(argv[optind]) + 5));
+				char *name = malloc(strlen(argv[optind]) + 5);
 				strcpy(name, argv[optind]);
 				strcat(name, ".rz");
 				f_dst = write_open(name, is_forced);
