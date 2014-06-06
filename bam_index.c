6801e6d76f17da3b184d547e3984ae3bc7f69df6
John Marshall <jm18@sanger.ac.uk>
2014-06-06 14:47:36 +0100

    Add samtools index options to select BAI or CSI
    Specifying the output filename remains unimplemented until htslib has
    API functions that allow it, at which point -o FILE should also be added.
    (Fixes #116 by allowing choice of index formats.)

--- bam_index.c
+++ bam_index.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
+#include <unistd.h>
 
 #define BAM_LIDX_SHIFT    14
 
@@ -14,14 +15,38 @@ int bam_index_build2(const char *fn, const char *_fnidx)
 	abort();
 }
 
+static void index_usage(FILE *fp)
+{
+	fprintf(fp,
+"Usage: samtools index [-bc] [-m INT] <in.bam> [out.index]\n"
+"Options:\n"
+"  -b       Generate BAI-format index for BAM files [default]\n"
+"  -c       Generate CSI-format index for BAM files\n"
+"  -m INT   Set minimum interval size for CSI indices to 2^INT [%d]\n", BAM_LIDX_SHIFT);
+}
+
 int bam_index(int argc, char *argv[])
 {
-	if (argc < 2) {
-		fprintf(stderr, "Usage: samtools index <in.bam> [out.index]\n");
+	int csi = 0;
+	int min_shift = BAM_LIDX_SHIFT;
+	int c;
+
+	while ((c = getopt(argc, argv, "bcm:")) >= 0)
+		switch (c) {
+		case 'b': csi = 0; break;
+		case 'c': csi = 1; break;
+		case 'm': csi = 1; min_shift = atoi(optarg); break;
+		default:
+			index_usage(stderr);
+			return 1;
+		}
+
+	if (optind == argc) {
+		index_usage(stdout);
 		return 1;
 	}
-	if (argc >= 3) bam_index_build2(argv[1], argv[2]);
-	else bam_index_build(argv[1], BAM_LIDX_SHIFT);
+	if (argc - optind > 1) bam_index_build2(argv[optind], argv[optind+1]);
+	else bam_index_build(argv[optind], csi? min_shift : 0);
 	return 0;
 }
 
