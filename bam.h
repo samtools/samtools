02a6359a893d2f61849435dc43e4c47f356ce8f9
John Marshall <jm18@sanger.ac.uk>
2014-07-09 10:24:37 +0100

    Update copyright years and reformat licensing boilerplate
    (There are also small contributions to bam.h from Nils Homer (whose
    contribution has since been moved to bam_tview.h) and Dan Kortschak
    that are not legally significant for copyright purposes.)

--- bam.h
+++ bam.h
@@ -1,29 +1,27 @@
-/* The MIT License
+/*  bam.h -- BAM API.
 
-   Copyright (c) 2008-2010 Genome Research Ltd (GRL).
+    Copyright (C) 2008-2014 Genome Research Ltd.
+    Portions copyright (C) 2010-2012 Broad Institute.
 
-   Permission is hereby granted, free of charge, to any person obtaining
-   a copy of this software and associated documentation files (the
-   "Software"), to deal in the Software without restriction, including
-   without limitation the rights to use, copy, modify, merge, publish,
-   distribute, sublicense, and/or sell copies of the Software, and to
-   permit persons to whom the Software is furnished to do so, subject to
-   the following conditions:
+    Author: Heng Li <lh3@sanger.ac.uk>
 
-   The above copyright notice and this permission notice shall be
-   included in all copies or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-   SOFTWARE.
-*/
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-/* Contact: Heng Li <lh3@sanger.ac.uk> */
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.  */
 
 #ifndef BAM_BAM_H
 #define BAM_BAM_H
