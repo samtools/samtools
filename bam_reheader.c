3b266baa1354e8b9911553db1d33220d5d70ab23
James Bonfield <jkb@sanger.ac.uk>
2015-06-17 11:41:50 +0100

    Rewrite samtools reheader in terms of htslib API (instead of bam.h).

--- bam_reheader.c
+++ bam_reheader.c
@@ -26,21 +26,21 @@ DEALINGS IN THE SOFTWARE.  */
 #include <stdio.h>
 #include <stdlib.h>
 #include "htslib/bgzf.h"
-#include "bam.h"
+#include "htslib/sam.h"
 
 #define BUF_SIZE 0x10000
 
-int bam_reheader(BGZF *in, const bam_header_t *h, int fd)
+int bam_reheader(BGZF *in, const bam_hdr_t *h, int fd)
 {
     BGZF *fp;
-    bam_header_t *old;
+    bam_hdr_t *old;
     ssize_t len;
     uint8_t *buf;
     if (in->is_write) return -1;
     buf = malloc(BUF_SIZE);
-    old = bam_header_read(in);
+    old = bam_hdr_read(in);
     fp = bgzf_fdopen(fd, "w");
-    bam_header_write(fp, h);
+    bam_hdr_write(fp, h);
     if (in->block_offset < in->block_length) {
         bgzf_write(fp, in->uncompressed_block + in->block_offset, in->block_length - in->block_offset);
         bgzf_flush(fp);
@@ -55,19 +55,19 @@ int bam_reheader(BGZF *in, const bam_header_t *h, int fd)
 
 int main_reheader(int argc, char *argv[])
 {
-    bam_header_t *h;
+    bam_hdr_t *h;
     BGZF *in;
     if (argc != 3) {
         fprintf(stderr, "Usage: samtools reheader <in.header.sam> <in.bam>\n");
         return 1;
     }
     { // read the header
-        tamFile fph = sam_open(argv[1]);
+        samFile *fph = sam_open(argv[1], "r");
         if (fph == 0) {
             fprintf(stderr, "[%s] fail to read the header from %s.\n", __func__, argv[1]);
             return 1;
         }
-        h = sam_header_read(fph);
+        h = sam_hdr_read(fph);
         sam_close(fph);
     }
     in = strcmp(argv[2], "-")? bgzf_open(argv[2], "r") : bgzf_fdopen(fileno(stdin), "r");
