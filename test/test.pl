b5d6a437839052f0f43f6ee60b0405c7759ba4c3
James Bonfield <jkb@sanger.ac.uk>
2015-06-25 09:28:27 +0100

    Fixed broken test.
    Also improved test harness to report the errors produced when a
    command fails to run.  Otherwise it's very hard to tell why a Travis
    build didn't work unless we can reproduce it (as we could this time
    via a fresh checkout).

--- test/test.pl
+++ test/test.pl
@@ -164,7 +164,7 @@ sub test_cmd
     print "\t$args{cmd}\n";
 
     my ($ret,$out,$err) = _cmd("$args{cmd}");
-    if ( $args{want_fail}? ($ret == 0) : ($ret != 0) ) { failed($opts,%args,msg=>$test); return; }
+    if ( $args{want_fail}? ($ret == 0) : ($ret != 0) ) { failed($opts,%args,msg=>$test,reason=>"ERR: $err"); return; }
     if ( $$opts{redo_outputs} && -e "$$opts{path}/$args{out}" )
     {
         rename("$$opts{path}/$args{out}","$$opts{path}/$args{out}.old");
@@ -2284,12 +2284,12 @@ sub test_reheader
     test_cmd($opts,
 	     out=>'reheader/3_view1.sam.expected',
 	     err=>'reheader/3_view1.sam.expected.err',
-	     cmd=>"$$opts{bin}/samtools reheader --in-place $$opts{path}/reheader/hdr.sam $fn.v21.cram && $$opts{bin}/samtools view -h $fn.cram | perl -pe 's/\tVN:.*//'",
+	     cmd=>"$$opts{bin}/samtools reheader --in-place $$opts{path}/reheader/hdr.sam $fn.v21.cram && $$opts{bin}/samtools view -h $fn.v21.cram | perl -pe 's/\tVN:.*//'",
 	     expect_fail=>0);
 
     test_cmd($opts,
 	     out=>'reheader/3_view1.sam.expected',
 	     err=>'reheader/3_view1.sam.expected.err',
-	     cmd=>"$$opts{bin}/samtools reheader --in-place $$opts{path}/reheader/hdr.sam $fn.v30.cram && $$opts{bin}/samtools view -h $fn.cram | perl -pe 's/\tVN:.*//'",
+	     cmd=>"$$opts{bin}/samtools reheader --in-place $$opts{path}/reheader/hdr.sam $fn.v30.cram && $$opts{bin}/samtools view -h $fn.v30.cram | perl -pe 's/\tVN:.*//'",
 	     expect_fail=>0);
 }
