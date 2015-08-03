1278126df2a9422fbde53abee6d2606f89d261b4
John Marshall <jm18@sanger.ac.uk>
2015-08-03 13:31:55 +0100

    Preserve TAB characters in command to be executed
    Current dash (and apparently whatever bash version travis-osx uses)
    read -r translates \t etc escapes into control characters.  Ensure
    that these survive intact rather than evaporating into spaces: for
    uses like "awk -v OFS='<tab>'" the difference is crucial.

--- test/mpileup/regression.sh
+++ test/mpileup/regression.sh
@@ -114,14 +114,14 @@ regtest() {
             if [ "`expr \"$cmd\" : '.*\$fmt'`" != 0 -a "$samtools" != "./samtools-0.1.19" ]
             then
                 _cmd=`echo $cmd | sed 's/\$fmt/bam/'`
-                run_test $p $o $_cmd
+                run_test $p $o "$_cmd"
                 #_cmd=`echo $cmd | sed 's/\$fmt/sam/'`
-                #run_test $p $o $_cmd
+                #run_test $p $o "$_cmd"
                 _cmd=`echo $cmd | sed 's/\$fmt/cram/'`
-                run_test $p $o $_cmd
+                run_test $p $o "$_cmd"
             else
                 _cmd=`echo $cmd | sed 's/\$fmt/bam/'`
-                run_test $p $o $_cmd
+                run_test $p $o "$_cmd"
             fi
             ;;
         esac
