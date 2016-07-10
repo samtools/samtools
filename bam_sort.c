d50d83d79f76e667340dd7682a867d1b67d32299
Nathan T. Weeks <weeks@iastate.edu>
2016-07-10 09:40:20 -0700

    Remove zero-initialization of bam1_t b to quell warning in gcc < 4.9

--- bam_sort.c
+++ bam_sort.c
@@ -1741,7 +1741,7 @@ int bam_sort_core_ext(int is_by_qname, const char *fn, const char *prefix,
     size_t max_k, k, max_mem;
     bam_hdr_t *header = NULL;
     samFile *fp;
-    bam1_t b = {0}, **buf;
+    bam1_t b, **buf;
     uint8_t *bam_mem, *bam_mem_offset;
 
     if (n_threads < 2) n_threads = 1;
