af75e548739420d4b58da8f3f8a70a8e8c9fc0de
bowhan <bowhan@icloud.com>
2017-02-27 18:35:22 -0500

    first working edition

--- bam_index.c
+++ bam_index.c
@@ -24,7 +24,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.  */
 
-#include <config.h>
+#include "config.h"
 
 #include <htslib/hts.h>
 #include <htslib/sam.h>
--- bam_lpileup.c
+++ bam_lpileup.c
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.  */
 
-#include <config.h>
+#include "config.h"
 
 #include <stdlib.h>
 #include <stdio.h>
