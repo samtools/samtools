samtools/samtools

commit 33987e16cebd63b990ec6bf651d4c4874bdee267

Added -i option for fastq

sam_view.c
@@ -683,6 +683,7 @@ static void bam2fq_usage(FILE *to, const char *command)
     fq ? "FASTQ" : "FASTA");
     if (fq) fprintf(to,
 "  -v INT               default quality score if not given in file [1]\n"
+"  -i                   add Illumina Casava 1.8 format entry to header (eg 1:N:0:ATCACG)\n"
 "  --i1 FILE            write first index reads to FILE\n"
 "  --i2 FILE            write second index reads to FILE\n"
 "  --barcode-tag TAG    Barcode tag [default: " DEFAULT_BARCODE_TAG "]\n"
@@ -706,7 +707,7 @@ typedef struct bam2fq_opts {
     char *fnse;
     char *fnr[3];
     char *fn_input; // pointer to input filename in argv do not free
-    bool has12, has12always, use_oq, copy_tags;
+    bool has12, has12always, use_oq, copy_tags, illumina_tag;
     int flag_on, flag_off, flag_alloff;
     sam_global_args ga;
     fastfile filetype;
@@ -724,11 +725,12 @@ typedef struct bam2fq_state {
     FILE *fpr[3];
     FILE *fpi[2];
     bam_hdr_t *h;
-    bool has12, use_oq, copy_tags;
+    bool has12, use_oq, copy_tags, illumina_tag;
     int flag_on, flag_off, flag_alloff;
     fastfile filetype;
     int def_qual;
     klist_t(ktaglist) *taglist;
+    char *index_sequence;
 } bam2fq_state_t;
 
 /*
@@ -857,6 +859,38 @@ static bool copy_tag(const char *tag, const bam1_t *rec, kstring_t *linebuf)
     return true;
 }
 
+static void insert_index_sequence_into_linebuf(char *index_sequence, kstring_t *linebuf, bam1_t *rec)
+{
+    int n;
+
+    if (!index_sequence) return;
+
+    kstring_t new = {0,0,NULL};
+    if (linebuf->s) {
+        char *s = strchr(linebuf->s, '\n');
+        if (s) {
+            *s = 0;
+            kputs(linebuf->s, &new);
+            kputc(' ', &new);
+            readpart readpart = which_readpart(rec);
+            if (readpart == READ_1) kputc('1', &new);
+            else if (readpart == READ_2) kputc('2', &new);
+            else kputc('0', &new);
+
+            kputc(':', &new);
+            if (rec->core.flag & BAM_FQCFAIL) kputc('Y', &new);
+            else                              kputc('N', &new);
+
+            kputs(":0:", &new);
+            kputs(index_sequence, &new);
+            kputc('\n', &new);
+            kputs(s+1, &new);
+            free(ks_release(linebuf)); kputs(new.s,linebuf);
+            free(ks_release(&new));
+        }
+    }
+}
+
 static bool make_fq_line(const bam1_t *rec, char *seq, char *qual, kstring_t *linebuf, const bam2fq_state_t *state)
 {
     int i;
@@ -913,7 +947,7 @@ static bool make_fq_line(const bam1_t *rec, char *seq, char *qual, kstring_t *li
 /*
  * Create FASTQ lines from the barcode tag using the index-format 
  */
-static bool tags2fq(bam1_t *rec, const bam2fq_state_t *state, const bam2fq_opts_t* opts)
+static bool tags2fq(bam1_t *rec, bam2fq_state_t *state, const bam2fq_opts_t* opts)
 {
     uint8_t *p;
     char *ifmt = opts->index_format;
@@ -921,6 +955,7 @@ static bool tags2fq(bam1_t *rec, const bam2fq_state_t *state, const bam2fq_opts_
     char *qual = NULL;
     int file_number = 0;
     kstring_t linebuf = { 0, 0, NULL }; // Buffer
+
 
     // read barcode tag
     p = bam_aux_get(rec,opts->barcode_tag);
@@ -964,7 +999,10 @@ static bool tags2fq(bam1_t *rec, const bam2fq_state_t *state, const bam2fq_opts_
         }
 
         if (action=='i' && *sub_tag && state->fpi[file_number]) {
+            //if (file_number==0) state->index_sequence = strdup(sub_tag);    // we're going to need this later...
+            state->index_sequence = strdup(sub_tag);    // we're going to need this later...
             if (!make_fq_line(rec, sub_tag, sub_qual, &linebuf, state)) return false;
+            if (state->illumina_tag) insert_index_sequence_into_linebuf(state->index_sequence, &linebuf, rec);
             fputs(linebuf.s, state->fpi[file_number++]);
         }
         free(sub_qual); free(sub_tag);
@@ -1046,7 +1084,7 @@ static bool parse_opts(int argc, char *argv[], bam2fq_opts_t** opts_out)
         {"quality-tag", required_argument, NULL, 'q'},
         { NULL, 0, NULL, 0 }
     };
-    while ((c = getopt_long(argc, argv, "0:1:2:f:F:G:nNOs:tT:v:@:", lopts, NULL)) > 0) {
+    while ((c = getopt_long(argc, argv, "0:1:2:f:F:G:niNOs:tT:v:@:", lopts, NULL)) > 0) {
         switch (c) {
             case 'b': opts->barcode_tag = strdup(optarg); break;
             case 'q': opts->quality_tag = strdup(optarg); break;
@@ -1064,6 +1102,7 @@ static bool parse_opts(int argc, char *argv[], bam2fq_opts_t** opts_out)
             case 'O': opts->use_oq = true; break;
             case 's': opts->fnse = optarg; break;
             case 't': opts->copy_tags = true; break;
+            case 'i': opts->illumina_tag = true; break;
             case 'T': opts->extra_tags = strdup(optarg); break;
             case 'v': opts->def_qual = atoi(optarg); break;
             case '?': bam2fq_usage(stderr, argv[0]); free_opts(opts); return false;
@@ -1161,9 +1200,11 @@ static bool init_state(const bam2fq_opts_t* opts, bam2fq_state_t** state_out)
     state->flag_alloff = opts->flag_alloff;
     state->has12 = opts->has12;
     state->use_oq = opts->use_oq;
+    state->illumina_tag = opts->illumina_tag;
     state->copy_tags = opts->copy_tags;
     state->filetype = opts->filetype;
     state->def_qual = opts->def_qual;
+    state->index_sequence = NULL;
 
     state->taglist = kl_init(ktaglist);
     if (opts->extra_tags) {
@@ -1263,6 +1304,7 @@ static bool destroy_state(const bam2fq_opts_t *opts, bam2fq_state_t *state, int*
         }
     }
     kl_destroy(ktaglist,state->taglist);
+    free(state->index_sequence);
     free(state);
     return valid;
 }
@@ -1276,8 +1318,10 @@ static inline bool filter_it_out(const bam1_t *b, const bam2fq_state_t *state)
 
 }
 
-static bool bam2fq_mainloop_singletontrack(bam2fq_state_t *state, bam2fq_opts_t* opts)
+static bool bam2fq_mainloop(bam2fq_state_t *state, bam2fq_opts_t* opts)
 {
+    int n;
+    bam1_t *records[3];
     bam1_t* b = bam_init1();
     char *current_qname = NULL;
     int64_t n_reads = 0, n_singletons = 0; // Statistics
@@ -1298,11 +1342,13 @@ static bool bam2fq_mainloop_singletontrack(bam2fq_state_t *state, bam2fq_opts_t*
 
         if (at_eof || !current_qname || (strcmp(current_qname, bam_get_qname(b)) != 0)) {
             if (current_qname) {
+                if (state->illumina_tag) for (n=0; n<3; n++) insert_index_sequence_into_linebuf(state->index_sequence, &linebuf[n], records[n]);
+                free(state->index_sequence); state->index_sequence = NULL;
                 if (score[1] > 0 && score[2] > 0) {
                     // print linebuf[1] to fpr[1], linebuf[2] to fpr[2]
                     if (fputs(linebuf[1].s, state->fpr[1]) == EOF) { valid = false; break; }
                     if (fputs(linebuf[2].s, state->fpr[2]) == EOF) { valid = false; break; }
-                } else if (score[1] > 0 || score[2] > 0) {
+                } else if ((score[1] > 0 || score[2] > 0) && state->fpse) {
                     // print whichever one exists to fpse
                     if (score[1] > 0) {
                         if (fputs(linebuf[1].s, state->fpse) == EOF) { valid = false; break; }
@@ -1327,12 +1373,13 @@ static bool bam2fq_mainloop_singletontrack(bam2fq_state_t *state, bam2fq_opts_t*
         // Prefer a copy of the read that has base qualities
         int b_score = bam_get_qual(b)[0] != 0xff? 2 : 1;
         if (b_score > score[which_readpart(b)]) {
+            if (state->fpi[0]) if (!tags2fq(b, state, opts)) return false;
+            records[which_readpart(b)] = b;
             if(!bam1_to_fq(b, &linebuf[which_readpart(b)], state)) {
                 fprintf(stderr, "[%s] Error converting read to FASTA/Q\n", __func__);
                 return false;
             }
             score[which_readpart(b)] = b_score;
-            if (state->fpi[0]) tags2fq(b, state, opts);
         }
     }
     if (!valid)
@@ -1350,31 +1397,6 @@ static bool bam2fq_mainloop_singletontrack(bam2fq_state_t *state, bam2fq_opts_t*
     return valid;
 }
 
-static bool bam2fq_mainloop(bam2fq_state_t *state, bam2fq_opts_t* opts)
-{
-    // process a name collated BAM into fastq
-    bam1_t* b = bam_init1();
-    if (b == NULL) {
-        perror(NULL);
-        return false;
-    }
-    int64_t n_reads = 0; // Statistics
-    kstring_t linebuf = { 0, 0, NULL }; // Buffer
-    while (sam_read1(state->fp, state->h, b) >= 0) {
-        if (filter_it_out(b, state)) continue;
-        ++n_reads;
-
-        if (!bam1_to_fq(b, &linebuf, state)) return false;
-        fputs(linebuf.s, state->fpr[which_readpart(b)]);
-        if (state->fpi[0]) tags2fq(b, state, opts);
-    }
-    free(linebuf.s);
-    bam_destroy1(b);
-
-    fprintf(stderr, "[M::%s] processed %" PRId64 " reads\n", __func__, n_reads);
-    return true;
-}
-
 int main_bam2fq(int argc, char *argv[])
 {
     int status = EXIT_SUCCESS;
@@ -1386,11 +1408,7 @@ int main_bam2fq(int argc, char *argv[])
 
     if (!init_state(opts, &state)) return EXIT_FAILURE;
 
-    if (state->fpse) {
-        if (!bam2fq_mainloop_singletontrack(state,opts)) status = EXIT_FAILURE;
-    } else {
-        if (!bam2fq_mainloop(state,opts)) status = EXIT_FAILURE;
-    }
+    if (!bam2fq_mainloop(state,opts)) status = EXIT_FAILURE;
 
     if (!destroy_state(opts, state, &status)) return EXIT_FAILURE;
     sam_global_args_free(&opts->ga);

test/test.pl
@@ -2247,6 +2247,10 @@ sub test_bam2fq
     test_cmd($opts, out=>'bam2fq/2.stdout.expected', out_map=>{'1.fq' => 'bam2fq/6.1.fq.expected', '2.fq' => 'bam2fq/6.2.fq.expected', 's.fq' => 'bam2fq/6.s.fq.expected'}, cmd=>"$$opts{bin}/samtools fastq @$threads -N -t -s $$opts{path}/s.fq -1 $$opts{path}/1.fq -2 $$opts{path}/2.fq $$opts{path}/dat/bam2fq.005.sam");
     # -T flag
     test_cmd($opts, out=>'bam2fq/2.stdout.expected', out_map=>{'1.fq' => 'bam2fq/7.1.fq.expected', '2.fq' => 'bam2fq/7.2.fq.expected', 's.fq' => 'bam2fq/7.s.fq.expected'}, cmd=>"$$opts{bin}/samtools fastq @$threads -N -t -T MD,ia -s $$opts{path}/s.fq -1 $$opts{path}/1.fq -2 $$opts{path}/2.fq $$opts{path}/dat/bam2fq.005.sam");
+    # -i flag with no index
+    test_cmd($opts, out=>'bam2fq/2.stdout.expected', out_map=>{'1.fq' => 'bam2fq/7.1.fq.expected', '2.fq' => 'bam2fq/7.2.fq.expected', 's.fq' => 'bam2fq/7.s.fq.expected'}, cmd=>"$$opts{bin}/samtools fastq @$threads -N -t -i -T MD,ia -s $$opts{path}/s.fq -1 $$opts{path}/1.fq -2 $$opts{path}/2.fq $$opts{path}/dat/bam2fq.005.sam");
+    # -i flag with index
+    test_cmd($opts, out=>'bam2fq/2.stdout.expected', out_map=>{'1.fq' => 'bam2fq/8.1.fq.expected', '2.fq' => 'bam2fq/8.2.fq.expected', 's.fq' => 'bam2fq/8.s.fq.expected', 'i.fq' => 'bam2fq/8.i.fq.expected'}, cmd=>"$$opts{bin}/samtools fastq @$threads --barcode-tag BC -i --index-format 'n2i2' --i1 $$opts{path}/i.fq -s $$opts{path}/s.fq -1 $$opts{path}/1.fq -2 $$opts{path}/2.fq $$opts{path}/dat/bam2fq.004.sam");
 }
 
 sub test_depad

samtools.1
@@ -1440,6 +1440,9 @@ present in the FLAG field.
 can be specified in hex by beginning with `0x' (i.e. /^0x[0-9A-F]+/)
 or in octal by beginning with `0' (i.e. /^0[0-7]+/) [0].
 .TP 8
+.B -i
+add Illumina Casava 1.8 format entry to header (eg 1:N:0:ATCACG)
+.TP 8
 .B --i1 FILE
 write first index reads to FILE
 .TP 8

test/bam2fq/8.1.fq.expected
@@ -1,0 +1,68 @@
+@ref1_grp1_p001 1:N:0:GT
+CGAGCTCGGT
++
+!!!!!!!!!!
+@ref1_grp1_p002 1:N:0:TT
+CTCGGTACCC
++
+##########
+@ref1_grp1_p003
+GTACCCGGGG
++
+%%%%%%%%%%
+@ref1_grp1_p004
+CCGGGGATCC
++
+''''''''''
+@ref1_grp1_p005
+GGATCCTCTA
++
+))))))))))
+@ref1_grp1_p006
+CCTCTAGAGT
++
+++++++++++
+@ref1_grp2_p001 1:N:0:CA
+AGCTCGGTAC
++
+""""""""""
+@ref1_grp2_p002
+CGGTACCCGG
++
+$$$$$$$$$$
+@ref1_grp2_p003
+ACCCGGGGAT
++
+&&&&&&&&&&
+@ref1_grp2_p004
+GGGGATCCTC
++
+((((((((((
+@ref1_grp2_p005
+ATCCTCTAGA
++
+**********
+@ref1_grp2_p006
+TCTAGAGTCG
++
+,,,,,,,,,,
+@ref2_grp3_p001
+GTGACACTATAGAAT
++
+~~~~~~~~~~~~~~~
+@ref2_grp3_p002
+CTGTTTCCTGTGTGA
++
+{{{{{{{{{{{{{{{
+@ref2_grp3_p003
+ACGTMRWSYKVHDBN
++
+0123456789abcd!
+@ref12_grp1_p001
+TGCAGGCATG
++
+AAAAAAAAAA
+@ref12_grp2_p001
+CAAGCTTGAG
++
+AAAAAAAAAA

test/bam2fq/8.2.fq.expected
@@ -1,0 +1,68 @@
+@ref1_grp1_p001 1:N:0:GT
+GTCGACTCTA
++
+----------
+@ref1_grp1_p002 1:N:0:TT
+GCAGGTCGAC
++
+//////////
+@ref1_grp1_p003
+GCCTGCAGGT
++
+1111111111
+@ref1_grp1_p004
+GCATGCCTGC
++
+3333333333
+@ref1_grp1_p005
+GCTTGCATGC
++
+5555555555
+@ref1_grp1_p006
+TCAAGCTTGC
++
+7777777777
+@ref1_grp2_p001 1:N:0:CA
+AGGTCGACTC
++
+..........
+@ref1_grp2_p002
+CTGCAGGTCG
++
+0000000000
+@ref1_grp2_p003
+ATGCCTGCAG
++
+2222222222
+@ref1_grp2_p004
+TTGCATGCCT
++
+4444444444
+@ref1_grp2_p005
+AAGCTTGCAT
++
+6666666666
+@ref1_grp2_p006
+ACTCAAGCTT
++
+8888888888
+@ref2_grp3_p001
+CTGTTTCCTGTGTGA
++
+|||||||||||||||
+@ref2_grp3_p002
+CGCCAAGCTATTTAG
++
+}}}}}}}}}}}}}}}
+@ref2_grp3_p003
+ACGTMRWSYKVHDBN
++
+0123456789abcd!
+@ref12_grp1_p001
+CACTATAGAA
++
+BBBBBBBBBB
+@ref12_grp2_p001
+ATTTAGGTGA
++
+BBBBBBBBBB

test/bam2fq/8.i.fq.expected
@@ -1,0 +1,12 @@
+@ref1_grp1_p001 1:N:0:GT
+GT
++
+""
+@ref1_grp1_p002 1:N:0:TT
+TT
++
+""
+@ref1_grp2_p001 1:N:0:CA
+CA
++
+cd

test/bam2fq/8.s.fq.expected
@@ -1,0 +1,8 @@
+@ref1_grp2_p002a
+CGGTACCCGG
++
+$$$$$$$$$$
+@unaligned_grp3_p001
+CACTCGTTCATGACG
++
+0123456789abcde

commit 6c87075a5d9ded7a65a7c0847e7ceb18e09c0bb0

Fix failure of bam2fq to write all reads on uncollated input
If the input to bam2fq is not collated by name, many reads will
look like they are single-ended because the paired reads may
not immediately follow each other.  Commit 33987e16 caused these
to be dropped if the -s flag was not used.  This restores the
previous behaviour where the records are written to the READ1 or
READ2 output file as appropriate.

Fixes #703

sam_view.c
@@ -1435,14 +1435,22 @@ static bool bam2fq_mainloop(bam2fq_state_t *state, bam2fq_opts_t* opts)
                     // print linebuf[1] to fpr[1], linebuf[2] to fpr[2]
                     if (bgzf_write(state->fpr[1], linebuf[1].s, linebuf[1].l) < 0) { valid = false; break; }
                     if (bgzf_write(state->fpr[2], linebuf[2].s, linebuf[2].l) < 0) { valid = false; break; }
-                } else if ((score[1] > 0 || score[2] > 0) && state->fpse) {
-                    // print whichever one exists to fpse
-                    if (score[1] > 0) {
-                        if (bgzf_write(state->fpse, linebuf[1].s, linebuf[1].l) < 0) { valid = false; break; }
+                } else if (score[1] > 0 || score[2] > 0) {
+                    if (state->fpse) {
+                        // print whichever one exists to fpse
+                        if (score[1] > 0) {
+                            if (bgzf_write(state->fpse, linebuf[1].s, linebuf[1].l) < 0) { valid = false; break; }
+                        } else {
+                            if (bgzf_write(state->fpse, linebuf[2].s, linebuf[2].l) < 0) { valid = false; break; }
+                        }
+                        ++n_singletons;
                     } else {
-                        if (bgzf_write(state->fpse, linebuf[2].s, linebuf[2].l) < 0) { valid = false; break; }
+                        if (score[1] > 0) {
+                            if (bgzf_write(state->fpr[1], linebuf[1].s, linebuf[1].l) < 0) { valid = false; break; }
+                        } else {
+                            if (bgzf_write(state->fpr[2], linebuf[2].s, linebuf[2].l) < 0) { valid = false; break; }
+                        }
                     }
-                    ++n_singletons;
                 }
                 if (score[0]) { // TODO: check this
                     // print linebuf[0] to fpr[0]

test/bam2fq/9.1.fq.expected
@@ -1,0 +1,28 @@
+@HS2000-355_269:2:1108:12969:26408
+CCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCT
++
+BCBFFFFFGGHHHJJJJIIJGIIIJIJIJIJII990?G=)?0?FH<GEF=CCFEE@G2=C?EE(;CB9;6@6(5??(559<?,9?AB239<2<8<928?8
+@HS2000-355_269:2:2312:2459:22449
+CCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCAAACCCTAACCCTAACCGTAACTCTAACCCTAACCCTATCTCAA
++
+@@@FFBFD8CDHHIBBHIIHIFHH3811)?:)?9):B)B#############################################################
+@HS2000-355_269:1:2307:11945:73421
+TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCAACCCTAACCCTAACCCTAACCATAACCCTAACCCTAACCCTAACCCTAACC
++
+@@@DFFDEHFHFHHGIJJGIJGHIGEGGHIIFEEHEGHHBBAA;?B@F(B(;;=)==66=?EE>?@7;;A##############################
+@HS2000-355_269:1:1114:7529:75240
+TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTACCCTAACCCTAACCCTCACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCTACCC
++
+@CCFFFFFDDAFB@GGHFGH@FHIFECBGAEC9CBBF?GFGDGIEGG>GCGI;=(8.6@=A)=???;?6;;66=2;;(,5<AB@5??#############
+@HS2000-355_269:2:2302:12695:6571
+CTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCACA
++
+@@@FBEFDDHHHHIIIIIFFAHGGHHGE??C8C;;FFB>;D>;@DEC@H(=FA@(==@)6=22)7;;?A>AA2;;;?@A#####################
+@HS2000-690_130:8:2104:17804:42012
+ACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCCACCCCCACCC
++
+CCCFFFFFHGDFHIIJJIIJJIGFHEHIIIJIGIEGDHHIIJJJHIHI=D(BC@FG2;=CEECH@CDF################################
+@HS2000-355_269:2:2213:5874:97462
+ACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTACCCCTAACCCTAACCCAAACCCTAACCCTAACCACAACCCACACCCATATCT
++
+@@@FDFADHDDBFG;CGB;BFHDHGGIG3CGGCC;9?GDF939BFG(B####################################################

test/bam2fq/9.2.fq.expected
@@ -1,0 +1,12 @@
+@HS2000-355_269:1:2307:11945:73421
+AGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGGTAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTGGGGTTGGGG
++
+?@@DBDBDFAAC;CBG<CGGII@GHCHGGEHAH@?DGDG2*??DFF*=BFCH'55@;??;6;@#####################################
+@HS2000-355_269:2:1108:12969:26408
+AGGGTTGGGTTAGGGTTGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGGTAGGGTTAGGGTTGGGGTTTGGGGTGGGGTTGGGGGT
++
+@@@FDDFFFFFADHIEHGII@G?GIG?DAHII9B@DFG8BBFCG)=BGEHAAA=AH76?@CC','5=B59(9?A##########################
+@HS2000-690_130:8:1215:11245:58923
+ACACTAACCCGAACCCTAACACAAACCCTAACCCTAACCCTAACCCTAACCCTAACGCGAACACCAACCATAACCCTAACCAACACACTAACGCTAACCC
++
+=>;AAA+AAB6AA12?22?@*:)?1??ABB######################################################################

test/dat/bam2fq.703.sam
@@ -1,0 +1,15 @@
+@HD VN:1.5  SO:coordinate
+@RG ID:LP6005441-DNA_C11    SM:LP6005441-DNA_C11
+@PG ID:bwa  PN:bwa  VN:0.7.15-r1140 CL:/bix_tools/bwakit-0.7.15/bwa mem -p -t30 -H@RG\tID:LP6005441-DNA_C11\tSM:LP6005441-DNA_C11 -C /sbgenomics/Projects/92c61d0f-a072-4169-b5c7-767d29e937ba/f7e48782-bb07-4283-9537-0a8fa5b7584e/alt-aware-bwa-mem-workflow_Alt_aware_BWA_MEM/hs38DH.fa -
+@PG ID:MarkDuplicates   VN:1.140(a81bc82e781dae05c922d1dbcee737334612399f_1444244284)   CL:picard.sam.markduplicates.MarkDuplicates INPUT=[/sbgenomics/Projects/92c61d0f-a072-4169-b5c7-767d29e937ba/f7e48782-bb07-4283-9537-0a8fa5b7584e/alt-aware-bwa-mem-workflow_Alt_aware_BWA_MEM/LP6005441-DNA_C11.hs38DH.aln.bam] OUTPUT=LP6005441-DNA_C11.hs38DH.aln.deduped.bam METRICS_FILE=LP6005441-DNA_C11.hs38DH.aln.metrics REMOVE_DUPLICATES=false ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT CREATE_INDEX=true    MAX_SEQUENCES_FOR_DISK_READ_ENDS_MAP=50000 MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=8000 SORTING_COLLECTION_SIZE_RATIO=0.25 PROGRAM_RECORD_ID=MarkDuplicates PROGRAM_GROUP_NAME=MarkDuplicates DUPLICATE_SCORING_STRATEGY=SUM_OF_BASE_QUALITIES READ_NAME_REGEX=[a-zA-Z0-9]+:[0-9]:([0-9]+):([0-9]+):([0-9]+).* OPTICAL_DUPLICATE_PIXEL_DISTANCE=100 VERBOSITY=INFO QUIET=false COMPRESSION_LEVEL=5 MAX_RECORDS_IN_RAM=500000 CREATE_MD5_FILE=false GA4GH_CLIENT_SECRETS=client_secrets.json  PN:MarkDuplicates
+@SQ	SN:chr1	LN:248956422
+HS2000-355_269:1:2307:11945:73421	147	chr1	10002	19	4S96M	=	10007	-91	CCCCAACCCCAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTACCCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCT	#####################################@;6;??;@55'HCFB=*FFD??*2GDGD?@HAHEGGHCHG@IIGGC<GBC;CAAFDBDBD@@?	XA:Z:chr1,-10106,7S39M1D32M1I21M,2;chr12,-10233,81M7D19M,10;chr20,+64287260,25M1I27M1D43M4S,4;chr18,-10001,24S21M1D55M,1;chr12_GL877875v1_alt,-233,81M7D19M,10;	MD:Z:5T37A52	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:2	AS:i:86	XS:i:78
+HS2000-355_269:2:1108:12969:26408	99	chr1	10004	0	100M	=	10044	123	CCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCT	BCBFFFFFGGHHHJJJJIIJGIIIJIJIJIJII990?G=)?0?FH<GEF=CCFEE@G2=C?EE(;CB9;6@6(5??(559<?,9?AB239<2<8<928?8	MD:Z:100	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:0	AS:i:100	XS:i:100
+HS2000-355_269:2:2312:2459:22449	99	chr1	10005	0	94M6S	=	10057	140	CCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCAAACCCTAACCCTAACCGTAACTCTAACCCTAACCCTATCTCAA	@@@FFBFD8CDHHIBBHIIHIFHH3811)?:)?9):B)B#############################################################	MD:Z:56T16C4C15	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:3	AS:i:79	XS:i:79
+HS2000-355_269:1:2307:11945:73421	99	chr1	10007	0	48M1D52M	=	10002	91	TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCAACCCTAACCCTAACCCTAACCATAACCCTAACCCTAACCCTAACCCTAACC	@@@DFFDEHFHFHHGIJJGIJGHIGEGGHIIFEEHEGHHBBAA;?B@F(B(;;=)==66=?EE>?@7;;A##############################	XA:Z:chr1,+10061,86M1D14M,2;chr12,+10044,48M1D52M,2;chr3,+10470,31M1I65M3S,2;chr4,-190122909,29M3D23M1D46M2S,4;chr18,+10029,66M1I33M,3;chr4,-190122830,52M1D11M1D37M,3;chr1,+10049,48M1D11M1I40M,3;chr1,-248946052,5M1D77M1D12M1D6M,4;chr20,-64287256,29M1I22M2D48M,4;chr1,+10126,21M1D79M,4;chr20,-64287310,52M3D30M1D18M,6;chr12_GL877875v1_alt,+44,48M1D52M,2;	MD:Z:48^T22C29	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:2	AS:i:88	XS:i:88	om:i:3
+HS2000-355_269:1:1114:7529:75240	1123	chr1	10007	0	37M1D60M3S	=	10366	457	TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTACCCTAACCCTAACCCTCACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCTACCC	@CCFFFFFDDAFB@GGHFGH@FHIFECBGAEC9CBBF?GFGDGIEGG>GCGI;=(8.6@=A)=???;?6;;66=2;;(,5<AB@5??#############	XA:Z:chr15,-101981029,100M,2;chr4,-190122948,100M,3;chrX,-156030570,62M1D38M,3;chr18,+10041,54M1I45M,3;chr4,+10078,100M,4;chr12,+10014,37M1D60M3S,3;chrX,-156030627,3S59M1D38M,3;chr18,-80262925,3S59M1D38M,3;chr3,+10392,65M1D35M,4;chr1,-248945958,14M1I85M,4;chr3,+10464,48M5D52M,7;chr3,+10605,80M1D10M1D10M,4;chr1,-248945908,10M1D13M1D77M,4;chr4,-190122897,46M3D16M1D38M,6;chr1,+180749,3S34M1D17M2D43M3S,4;chr1,-248946043,3S14M1D80M3S,4;chr4,+10001,2S54M2D44M,6;chr18,+10117,30M1D8M2D57M5S,5;chr19,-58607527,11S9M1D42M1D37M1S,3;chr3,+10000,4S33M1D23M1I23M16S,2;chr12_GL877875v1_alt,+14,37M1D60M3S,3;	MD:Z:37^A17A34T7	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:3	AS:i:80	XS:i:92
+HS2000-355_269:2:2302:12695:6571	99	chr1	10012	0	40M1I56M3S	=	10056	128	CTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCACA	@@@FBEFDDHHHHIIIIIFFAHGGHHGE??C8C;;FFB>;D>;@DEC@H(=FA@(==@)6=22)7;;?A>AA2;;;?@A#####################	MD:Z:96	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:1	AS:i:89	XS:i:89
+HS2000-690_130:8:2104:17804:42012	99	chr1	10021	0	88M12S	=	10256	322	ACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCCACCCCCACCC	CCCFFFFFHGDFHIIJJIIJJIGFHEHIIIJIGIEGDHHIIJJJHIHI=D(BC@FG2;=CEECH@CDF################################	XA:Z:chr22,-50808379,18S82M,0;chr4,-190122711,12S77M1I10M,1;chr12,+10232,85M15S,1;chr5,+11708,77M23S,0;chr18,-80262920,23S77M,0;chr21,-46699849,23M1D77M,5;chr13,-114354165,23S77M,0;chr18,+10131,16M1D12M1D72M,5;chr1,-248946173,82M4I6M2D8M,8;chr3,+10607,35M1I47M17S,2;chr3,+10136,1S30M1D6M1D18M1I33M11S,3;chr12_GL877875v1_alt,+232,85M15S,1;	MD:Z:76T5T5	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:2	AS:i:78	XS:i:82
+HS2000-355_269:2:2213:5874:97462	99	chr1	10027	0	81M19S	=	10070	110	ACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTACCCCTAACCCTAACCCAAACCCTAACCCTAACCACAACCCACACCCATATCT	@@@FDFADHDDBFG;CGB;BFHDHGGIG3CGGCC;9?GDF939BFG(B####################################################	XA:Z:chrX,-156030354,19S81M,2;chr12,+10232,76M24S,1;chr21,-46699875,19S81M,2;chr19,-58607537,21S79M,2;chr15,-101981059,19S29M1I51M,2;chr4,-190122978,19S29M1I51M,2;chr4,+10068,48M1I32M19S,2;chr1,-248945940,19S29M1I51M,2;chr1,-248946022,19S35M1D46M,3;chr3,+10458,4S44M1I15M1I16M19S,2;chr18,+10031,48M1I15M1I16M19S,3;chr3,+10518,53M1I27M19S,4;chr1,-248945689,37S63M,1;chr18,+10242,48M1I11M4D21M19S,6;chr3,-198173397,19S22M1I58M,4;chr18,+10462,48M1I11M4D21M19S,6;chr12_GL877875v1_alt,+232,76M24S,1;chr3_KI270784v1_alt,+62084,58M1I22M19S,4;	MD:Z:48A15T16	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:2	AS:i:71	XS:i:71
+HS2000-355_269:2:1108:12969:26408	147	chr1	10044	0	18S76M1D6M	=	10004	-123	ACCCCCAACCCCACCCCAAACCCCAACCCTAACCCTACCCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCAACCCTAACCCAACCCT	##########################A?9(95B=5','CC@?67HA=AAAHEGB=)GCFBB8GFD@B9IIHAD?GIG?G@IIGHEIHDAFFFFFDDF@@@	XA:Z:chr4,+190122818,6S76M18S,3;chr5,-10146,18S65M1D11M6S,3;chr5,-10206,18S65M1D11M6S,3;chr5,-10062,18S65M1D11M6S,3;chr5,-10134,18S65M1D11M6S,3;chr5,-10092,18S65M1D11M6S,3;chr5,-10074,18S65M1D11M6S,3;chr5,-10038,18S65M1D11M6S,3;chr5,-10044,18S65M1D11M6S,3;chr5,-10128,18S65M1D11M6S,3;chr5,-10014,18S65M1D11M6S,3;chr5,-10056,18S65M1D11M6S,3;chr5,-10116,18S65M1D11M6S,3;chr5,-10026,18S65M1D11M6S,3;chr5,-10176,18S65M1D11M6S,3;chr5,-10086,18S65M1D11M6S,3;chr5,-10104,18S65M1D11M6S,3;chr5,-10110,18S65M1D11M6S,3;chr5,-10080,18S65M1D11M6S,3;chr5,-10050,18S65M1D11M6S,3;chr5,-10230,18S65M1D11M6S,3;chr5,-10194,18S65M1D11M6S,3;chr5,-10152,18S65M1D11M6S,3;chr5,-10164,18S65M1D11M6S,3;chr5,-10002,18S65M1D11M6S,3;chr5,-10020,18S65M1D11M6S,3;chr5,-10158,18S65M1D11M6S,3;chr5,-10212,18S65M1D11M6S,3;chr5,-10224,18S65M1D11M6S,3;chr5,-10122,18S65M1D11M6S,3;chr5,-10182,18S65M1D11M6S,3;chr5,-10008,18S65M1D11M6S,3;chr5,-10188,18S65M1D11M6S,3;chr5,-10140,18S65M1D11M6S,3;chr5,-10170,18S65M1D11M6S,3;chr5,-10098,18S65M1D11M6S,3;chr5,-10032,18S65M1D11M6S,3;chr5,-10068,18S65M1D11M6S,3;chr5,-10218,18S65M1D11M6S,3;chr5,-10200,18S65M1D11M6S,3;chr3,-10590,26S68M1D6M,3;chr12,-10173,14S69M1D11M1D6M,5;chr10,+133787131,6S56M38S,0;chr11,-175728,28S66M6S,2;chr18,-9999,12S23M1D48M1D17M,6;chr12_GL877875v1_alt,-173,14S69M1D11M1D6M,5;	MD:Z:5T13A56^T6	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:3	AS:i:66	XS:i:61
+HS2000-690_130:8:1215:11245:58923	163	chr1	10048	0	3S59M1I18M19S	=	10162	177	ACACTAACCCGAACCCTAACACAAACCCTAACCCTAACCCTAACCCTAACCCTAACGCGAACACCAACCATAACCCTAACCAACACACTAACGCTAACCC	=>;AAA+AAB6AA12?22?@*:)?1??ABB######################################################################	XA:Z:chr22,-50808004,19S78M3S,7;chr4,-190123032,44S51M5S,2;chr4,+10120,11S70M19S,6;chr12,-133265107,44S53M3S,3;chr12,-133264993,44S53M3S,3;chr12,-133264939,44S53M3S,3;chr12,-133264957,44S53M3S,3;chr12,-133265035,44S53M3S,3;chr12,-133265011,44S53M3S,3;chr12,-133265059,44S53M3S,3;chr12,-133265023,44S53M3S,3;chr12,+10061,3S53M44S,3;chr12,-133265119,44S53M3S,3;chr12,-133265089,44S53M3S,3;chr12,-133265065,44S53M3S,3;chr12,-133264987,44S53M3S,3;chr12,-133265029,44S53M3S,3;chr12,-133265131,44S53M3S,3;chr12,-133265125,44S53M3S,3;chr12,-133264981,44S53M3S,3;chr12,-133265071,44S53M3S,3;chr12,-133265137,44S53M3S,3;chr12,-133265101,44S53M3S,3;chr12,-133265005,44S53M3S,3;chr12,-133265083,44S53M3S,3;chr12,-133265077,44S53M3S,3;chr12,-133265095,44S53M3S,3;chr12,-133264975,44S53M3S,3;chr12,-133264945,44S53M3S,3;chr18,-80263049,44S53M3S,3;chr12,-133264999,44S53M3S,3;chr12,-133264969,44S53M3S,3;chr12,-133265041,44S53M3S,3;chr12,-133264963,44S53M3S,3;chrX,-156030475,44S53M3S,3;chr12,-133265047,44S53M3S,3;chr12,-133265113,44S53M3S,3;chr12,-133265017,44S53M3S,3;chr12,-133265053,44S53M3S,3;chr12,-133264951,44S53M3S,3;chr5,+11557,22M1D59M6I13M,16;chr15,-101981093,13M6I59M1D22M,16;chr7,-159335918,44S34M1D19M3S,3;chr20,-64287329,44S34M2D22M,5;chr9,-2824049,44S35M21S,0;chr17,+113252,22M2D34M44S,5;chr17,+113129,3S49M48S,3;chr1,+18254357,22S34M44S,0;chr10,-133787156,44S34M22S,0;chr1,+180918,22S34M44S,0;chr1,+10255,22S34M44S,0;chr17_GL383563v3_alt,+53252,22M2D34M44S,5;chr17_GL383563v3_alt,+53129,3S49M48S,3;	MD:Z:7T9C1T33C1T9C11	PG:Z:MarkDuplicates	RG:Z:LP6005441-DNA_C11	NM:i:7	AS:i:40	XS:i:43

test/test.pl
@@ -61,7 +61,6 @@ test_reheader($opts);
 test_addrprg($opts);
 test_addrprg($opts, threads=>2);
 
-
 print "\nNumber of tests:\n";
 printf "    total            .. %d\n", $$opts{nok}+$$opts{nfailed}+$$opts{nxfail}+$$opts{nxpass};
 printf "    passed           .. %d\n", $$opts{nok};
@@ -2252,6 +2251,10 @@ sub test_bam2fq
     test_cmd($opts, out=>'bam2fq/2.stdout.expected', out_map=>{'1.fq' => 'bam2fq/7.1.fq.expected', '2.fq' => 'bam2fq/7.2.fq.expected', 's.fq' => 'bam2fq/7.s.fq.expected'}, cmd=>"$$opts{bin}/samtools fastq @$threads -N -t -i -T MD,ia -s $$opts{path}/s.fq -1 $$opts{path}/1.fq -2 $$opts{path}/2.fq $$opts{path}/dat/bam2fq.005.sam");
     # -i flag with index
     test_cmd($opts, out=>'bam2fq/2.stdout.expected', out_map=>{'1.fq' => 'bam2fq/8.1.fq.expected', '2.fq' => 'bam2fq/8.2.fq.expected', 's.fq' => 'bam2fq/8.s.fq.expected', 'i.fq' => 'bam2fq/8.i.fq.expected'}, cmd=>"$$opts{bin}/samtools fastq @$threads --barcode-tag BC -i --index-format 'n2i2' --i1 $$opts{path}/i.fq -s $$opts{path}/s.fq -1 $$opts{path}/1.fq -2 $$opts{path}/2.fq $$opts{path}/dat/bam2fq.004.sam");
+
+    # test for Issue #703 (failure to write all reads on uncollated input)
+    test_cmd($opts, out=>'bam2fq/2.stdout.expected', out_map=>{'1.fq' => 'bam2fq/9.1.fq.expected', '2.fq' => 'bam2fq/9.2.fq.expected'}, cmd=>"$$opts{bin}/samtools fastq @$threads -1 $$opts{path}/1.fq -2 $$opts{path}/2.fq $$opts{path}/dat/bam2fq.703.sam");
+
 }
 
 sub test_depad