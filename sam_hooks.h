7cd06e7ad2684fca2e86820d0d7ef8eafccd5ed4
lindenb <plindenbaum@yahoo.fr>
2019-02-28 17:38:48 +0100

    cont

--- sam_hooks.h
+++ sam_hooks.h
@@ -1,13 +1,23 @@
 #ifndef SAMTOOLS_HOOK_H
 #define SAMTOOLS_HOOK_H
 
-struct sam_hook_t {
+#include "htslib/sam.h"
+
+
+typedef struct sam_hook_t {
   int (*initialize)(struct sam_hook_t* ,const bam_hdr_t *);
   int (*accept)(struct sam_hook_t* ,const bam_hdr_t *, bam1_t *);
   int (*dispose)(struct sam_hook_t* );
-  };
+  char* (*get_name)(struct sam_hook_t* );
+  
+  /** next hoot in chained list */
+  struct sam_hook_t* next;
+  /* reserved for the hook */
+  void* reserved;
+  }SamHook, *SamHookPtr;
 
-struct sam_hook_t* hook_load_by_name(const char* name);
-
+SamHookPtr hook_load_by_name(SamHookPtr root,const char* name);
+void hook_dispose_all(SamHookPtr ptr);
+int hook_accept_all(SamHookPtr hook,const bam_hdr_t *, bam1_t*);
 #endif
 
