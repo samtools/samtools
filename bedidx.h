a89beb3f35bbfabe180a019a89c3a2993c9c325f
James Bonfield <jkb@sanger.ac.uk>
2026-01-14 09:55:10 +0000

    Remove the undocumented and unused bed_get function.
    Not only is this unused, it's also impossible to use within knowing
    the internals of the bed void* data structure.  One of the parameters
    is the numeric index into its internal hash table, but there is no API
    to return the size of this hash.
    
    It appeared with commit ce0492384, but was apparently unused even then.

--- bedidx.h
+++ bedidx.h
@@ -65,7 +65,6 @@ hts_reglist_t *bed_reglist(void *reg_hash, int filter, int *count_regs);
 
 int bed_overlap(const void *_h, const char *chr, hts_pos_t beg, hts_pos_t end);
 void *bed_hash_regions(void *reg_hash, char **regs, int first, int last, int *op);
-const char* bed_get(void *reg_hash, int index, int filter);
 void bed_unify(void *_h);
 
 #endif
