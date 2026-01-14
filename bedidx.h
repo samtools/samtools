2d908c3850c5204b1493557af0c3a0be3c232c68
James Bonfield <jkb@sanger.ac.uk>
2026-01-14 09:39:13 +0000

    Add some rudimentary comments to bedidx.h.
    This is incomplete, but at least covers some of the pitfalls I learnt
    while using it.  Specifically not to trust the returned
    hts_reglist_t->tid member and to expect unordered output.

--- bedidx.h
+++ bedidx.h
@@ -34,12 +34,38 @@ DEALINGS IN THE SOFTWARE.  */
 #define MIN(A,B) ( ( (A) < (B) ) ? (A) : (B) )
 #define MAX(A,B) ( ( (A) > (B) ) ? (A) : (B) )
 
+/*
+ * Bed file reading.  The main functions are bed_read and bed_destroy.
+ * The void* return value from bed_read is the same "void *reg_hash" used in
+ * bed_overlap, bed_hash_regions, bed_get and bed_reglist.
+ *
+ * Due to the data structures used, the order of chromosomes in the BED file
+ * is not preserved and iterators will return chromosomes in hash key order.
+ */
 void *bed_read(const char *fn);
 void bed_destroy(void *_h);
+
+/*
+ * bed_reglist will turn the bed file into a list of regions structured as
+ * an hts_reglist_t: an array with one entry per chromosome, with each
+ * element containing a further array of regions within that chromosome.
+ *
+ * Note however this function does not take a SAM or VCF header, so the
+ * hts_reglist_t "tid" member will be invalid.  Users are expected to perform
+ * this conversion themselves from the "chr" string member instead.  (See
+ * hts_itr_regions for example code which does this.)
+ *
+ * Use filtered = ALL (0) unless you have previously applied bed_hash_regions
+ * with reg_hash != NULL and *op == 0, in which case FILTERED may be used to
+ * perform an intersection of bed file and an array of region strings.
+ *
+ * Free using hts_reglist_free()
+ */
+hts_reglist_t *bed_reglist(void *reg_hash, int filter, int *count_regs);
+
 int bed_overlap(const void *_h, const char *chr, hts_pos_t beg, hts_pos_t end);
 void *bed_hash_regions(void *reg_hash, char **regs, int first, int last, int *op);
 const char* bed_get(void *reg_hash, int index, int filter);
-hts_reglist_t *bed_reglist(void *reg_hash, int filter, int *count_regs);
 void bed_unify(void *_h);
 
 #endif
