39d7cb72c444f7822610df44337664fcbc0204e1
Rob Davies <rmd+git@sanger.ac.uk>
2026-02-16 16:40:28 +0000

    Change isdigit cast in unclipped_other_start/end()
    Need to cast to unsigned to ensure all possible values are +ve.

--- bam.c
+++ bam.c
@@ -184,7 +184,7 @@ hts_pos_t unclipped_other_start(hts_pos_t op, char *cigar) {
     while (*c && *c != '*') {
         long num = 0;
 
-        if (isdigit((int)*c)) {
+        if (isdigit((unsigned char)*c)) {
             num = strtol(c, &c, 10);
         } else {
             num = 1;
@@ -236,7 +236,7 @@ hts_pos_t unclipped_other_end(int64_t op, char *cigar) {
     while (*c && *c != '*') {
         long num = 0;
 
-        if (isdigit((int)*c)) {
+        if (isdigit((unsigned char)*c)) {
             num = strtol(c, &c, 10);
         } else {
             num = 1;
